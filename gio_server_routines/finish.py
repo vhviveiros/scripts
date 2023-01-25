01b348fe192940908b27544f0cef6205ea0dbfc4
Victor Hugo <vh.viveiros@gmail.com>
2023-01-25 20:36:00 -0300

    ...

--- gio_server_routines/finish.py
+++ gio_server_routines/finish.py
@@ -2,4 +2,5 @@ from shared import stop_all_containers, start_stopped_containers, subprocess
 
 stop_all_containers(save=False)
 start_stopped_containers()
+subprocess.run(["netsh.exe", "winhttp", "reset", "proxy"])
 subprocess.run(["wsl.exe", "--shutdown"])
