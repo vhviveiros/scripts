ae7cad635998d3e1ee6a96367d59677d7fb18884
Victor Hugo <vh.viveiros@gmail.com>
2023-02-08 17:32:17 -0300

    added a shutdown option after finish

--- gio_scripts/finish.py
+++ gio_scripts/finish.py
@@ -1,6 +1,9 @@
+import sys
 from shared import stop_all_containers, start_stopped_containers, subprocess
 
 stop_all_containers(save=False)
 start_stopped_containers()
 subprocess.run(["netsh.exe", "winhttp", "reset", "proxy"])
+if (sys.argv.__contains__('-s')):
+    subprocess.run(["shutdown.exe", "-s", "-t", "10"])
 subprocess.run(["wsl.exe", "--shutdown"])
